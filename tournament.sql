65ded8cc9e78772f49c1b39e32abbaa8853290fa
Sajid Zaman <sajid-619@users.noreply.github.com>
2016-04-23 13:06:53 +0600

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -1,33 +1,71 @@
-DROP DATABASE IF EXISTS tournament; 
-CREATE DATABASE tournament; 
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+
+\c tournament
 
 DROP TABLE IF EXISTS players CASCADE;
-create table players (
-	player_id serial  PRIMARY KEY NOT NULL,
-	player_name varchar(100) NOT NULL
-	);
+DROP TABLE IF EXISTS tournaments CASCADE;
+DROP TABLE IF EXISTS tournament_games CASCADE;
+--DROP VIEW IF EXISTS matches CASCADE;
+--DROP VIEW IF EXISTS wins CASCADE;
+DROP VIEW IF EXISTS standings CASCADE;
 
-DROP TABLE IF EXISTS matches CASCADE;
-create table matches (
-	match_id serial PRIMARY KEY NOT NULL,
-	id_player_a integer NOT NULL,
-	id_player_b integer NOT NULL
-	);
 
-DROP TABLE IF EXISTS matchResults CASCADE;
-create table matchResults (
-	match_result_id serial PRIMARY KEY NOT NULL,
-	match_id integer NOT NULL,
-	player_id integer NOT NULL REFERENCES players(player_id),
-	match_win integer NOT NULL default 0,
-	match_loss integer NOT NULL default 0
-	);
+-- Stores player names with their ids. Names not unique
+CREATE TABLE players (
+						player_id serial primary key,
+						name varchar not null
+					);
 
-DROP VIEW IF EXISTS playerStandings CASCADE;
-CREATE VIEW playerStandings AS (
-	SELECT players.player_id AS id, players.player_name AS name, coalesce(playerSummary.wins,0) AS wins, coalesce(playerSummary.matches,0) AS matches  
-	FROM players LEFT JOIN (SELECT player_id, SUM(match_win) AS wins, COUNT(player_id) AS matches 
-		FROM matchResults GROUP BY player_id) playerSummary
-	ON players.player_id = playerSummary.player_id
-	ORDER BY playerSummary.wins DESC
-	);
+
+-- Lists players for each tournament.The player has a bye win or not.
+CREATE TABLE tournaments (
+  							tournament_name text,
+  							player_id integer references players,
+  							bye_win BOOLEAN DEFAULT false,
+  							PRIMARY KEY (tournament_name, player_id) 
+						);
+
+
+--Results of all matches for all tournament_games. Draw is supported.
+
+CREATE TABLE tournament_games (
+						tournament_name varchar,
+						game_id serial,
+						player1_id integer,
+						player2_id integer,
+						winner_id integer default null,
+						draw BOOLEAN default false,
+						CONSTRAINT winner CHECK((draw AND winner_id IS NULL) OR (NOT draw AND winner_id IS NOT NULL)),
+						PRIMARY KEY (tournament_name, game_id),
+						FOREIGN KEY (tournament_name, player1_id) references tournaments,
+						FOREIGN KEY (tournament_name, player2_id) references tournaments
+					);
+
+--CREATE TABLE tournament_records (
+
+--						tournament varchar references tournaments (name),
+--						players_id varchar references players (id),
+--					);
+--
+
+
+-- Aggregates data for each player in each tournament. Wins are calculated as 2 points per win, 1 per draw.
+CREATE VIEW standings AS 
+		SELECT tournaments.tournament_name, tournaments.player_id,
+		count(CASE WHEN (tournaments.player_id = tournament_games.player1_id OR tournaments.player_id = tournament_games.player2_id) THEN 1 END) AS matches,
+		count(CASE WHEN tournament_games.winner_id = tournaments.player_id THEN 1 END) AS wins,
+		count(CASE WHEN tournament_games.winner_id = tournaments.player_id THEN 1 END) AS draws,
+		count(CASE WHEN tournament_games.winner_id = tournaments.player_id THEN 2 WHEN tournament_games.draw = true THEN 1 END) AS points
+		FROM tournaments left join tournament_games
+		ON tournament_games.tournament_name = tournaments.tournament_name 
+		AND (tournaments.player_id = tournament_games.player1_id OR tournaments.player_id = tournament_games.player2_id)
+		GROUP BY tournaments.tournament_name, tournaments.player_id
+		ORDER BY tournament_name ASC, points DESC;
+
+
+
+--LEFT JOIN tournament_games 
+--ON tournaments.tournament_name = tournament_games.tournament_name 
+--AND (tournaments.player_id = tournament_games.player1_id OR tournaments.player_id = tournaments_games.player2_id)
+
